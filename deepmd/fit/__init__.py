ed234a8b90df0a49ccdec5aba905e11d49211c8a
Zhang-pchao <pz-zhang20@foxmail.com>
2023-06-18 11:25:33 +0200

    modify fit/init

--- deepmd/fit/__init__.py
+++ deepmd/fit/__init__.py
@@ -4,6 +4,9 @@ from .dipole import (
 from .dos import (
     DOSFitting,
 )
+from .avec import (
+    AVecFitting,
+)
 from .ener import (
     EnerFitting,
 )
@@ -19,6 +22,7 @@ __all__ = [
     "DipoleFittingSeA",
     "EnerFitting",
     "DOSFitting",
+    "AVecFitting",
     "GlobalPolarFittingSeA",
     "PolarFittingSeA",
     "Fitting",
